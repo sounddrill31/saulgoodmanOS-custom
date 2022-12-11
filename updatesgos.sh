db1aba34d4ea890e7282bc7676dd2ec0e2d88cba
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-12-11 19:56:01 +0530

    Update updatesgos.sh

--- updatesgos.sh
+++ updatesgos.sh
@@ -4,7 +4,8 @@ sudo apt update
 sudo apt install -y neofetch
 neofetch
 
-echo "Testing Over, Do you want to continue modifying your ubuntu unity into Saul Goodman OS?"
+echo "Testing Over, Do you want to continue modifying your ubuntu unity into Saul Goodman OS? You can run this in cubic(WIP) or on Bare Metal."
+echo "Disclaimer, This will remove snaps and snap packages, since I hate snaps."
 
 while true; do
 
