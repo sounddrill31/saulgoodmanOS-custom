d326b406f54bb2d0efebff770364a5c0ba6ec74e
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-12-11 19:31:54 +0530

    Update updatesgos.sh

--- updatesgos.sh
+++ updatesgos.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt install -y neofetch
 neofetch
 
 echo "Testing Over, Do you want to continue modifying your ubuntu unity into Saul Goodman OS?"
+
+read -p "Do you want to proceed? (yes/no) " yn
+
+case $yn in 
+	yes ) echo ok, we will proceed;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
