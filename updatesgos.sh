0bd775fcd084d231202d1c64df22a7421abe99a4
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-12-13 12:24:50 +0530

    Update updatesgos.sh

--- updatesgos.sh
+++ updatesgos.sh
@@ -33,7 +33,7 @@ cd Saulpapers
 #wget -O saulpaperback.png https://i.kym-cdn.com/photos/images/newsfeed/001/893/464/3e7.jpg
 #display -window root saulpaperback.png
 wget -O saulpaperfront.png https://i.kym-cdn.com/photos/images/newsfeed/001/893/472/c1e.png 
-sudo cp -v ~/Saulpapers/saulpaperfront.png "/usr/share/lxde/wallpapers/saulpaperfront.png"
+sudo cp -v ~/Saulpapers/saulpaperfront.png /usr/share/lxde/wallpapers/
 pcmanfm --set-wallpaper="/usr/share/lxde/wallpapers/saulpaperfront.png"
 
 #display -window root saulpaperfront.png
