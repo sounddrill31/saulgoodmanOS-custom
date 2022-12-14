23ec9229482a2ab4763af3859dba7bc4ed968d8c
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-12-14 10:39:28 +0530

    Update updatesgos.sh

--- updatesgos.sh
+++ updatesgos.sh
@@ -61,7 +61,7 @@ wget -O splash.png https://github.com/sounddrill31/saulgoodmanOS-custom/blob/mai
 sudo cp splash.png /usr/share/plymouth/themes/pix/saulsplash.png
 #sudo sed -i -r 's/theme_image = Image("splash.png");/theme_image = Image("saulsplash.png")/' /usr/share/plymouth/themes/pix/pix.script
 #sed -i 's/theme_image = Image("splash.png");/theme_image = Image("saulsplash.png");/g' /usr/share/plymouth/themes/pix/pix.script
-sed -i -r 's/splash.png/saulsplash.png/' /usr/share/plymouth/themes/pix/pix.script
+sudo sed -i -r 's/splash.png/saulsplash.png/' /usr/share/plymouth/themes/pix/pix.script
 
 #Menu Button
 
